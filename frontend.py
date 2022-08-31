e77e65603fd680533af29e380934d01605ec2229
nickL <nick@nickLs-MacBook-Air.local>
2022-08-30 23:40:39 -0500

    deploy

--- frontend.py
+++ frontend.py
@@ -4,6 +4,7 @@ import json
 
 
 def run():
+    PORT = "0.0.0.0"
     streamlit.title("Lets Analyze Text")
     sentence = streamlit.text_input("Type Sentence Here")
 
@@ -12,17 +13,17 @@ def run():
     }
 
     if streamlit.button("Analyze Text", key=1):
-        response = requests.post("http://0.0.0.0:8000/analyze_text", json=data)
+        response = requests.post(f"http://0.0.0.0:{PORT}/analyze_text", json=data)
         analyze = response.text
         streamlit.success(f"Here you go: {analyze}")
 
     if streamlit.button("Analyze Sentiment", key=2):
-        response = requests.post("http://0.0.0.0:8000/sentiment_analysis", json=data)
+        response = requests.post(f"http://0.0.0.0:{PORT}/sentiment_analysis", json=data)
         analyze = response.text
         streamlit.success(f"Here you go: {analyze}")
 
     if streamlit.button("Named Entity Recognition", key="3"):
-        response = requests.post("http://0.0.0.0:8000/entity_recognition", json=data)
+        response = requests.post(f"http://0.0.0.0:{PORT}/entity_recognition", json=data)
         analyze = response.text
         streamlit.success(f"Here you go: {analyze}")
 
