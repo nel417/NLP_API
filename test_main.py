07a946f21a1e17b7949cbbc427281455feacb4b0
nickL <nick@nickLs-MacBook-Air.local>
2022-10-17 15:22:58 -0500

    tests for spacy sentiment endpoint

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_get_entity():
     response = client.post(
         "/entity_recognition",
-        headers={"X-Token": "coneofsilence"},
+        headers={"content-type": "application/json"},
         json={"sentence": "Bill Gates"},
     )
 
@@ -22,3 +22,78 @@ def test_get_entity():
             }
         ]
     }
+
+
+def test_get_entity_empty():
+    response = client.post(
+        "/entity_recognition",
+        headers={"content-type": "application/json"},
+        json={"sentence": None},
+    )
+
+    assert response.status_code == 422
+
+
+def test_get_text_sentiment_neg():
+    response = client.post(
+        "/sentiment_analysis",
+        headers={"content-type": "application/json"},
+        json={"sentence": "I hate Chilis."},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+
+            "output": {
+                "Score": [
+                    -0.8
+                ],
+                "Label": [
+                    "Negative"
+                ],
+                "Positive words": [
+                    ""
+                ],
+                "Negative Words": [
+                    "hate"
+                ]
+            }
+
+    }
+
+
+def test_get_text_sentiment_pos():
+    response = client.post(
+        "/sentiment_analysis",
+        headers={"content-type": "application/json"},
+        json={"sentence": "I love Chilis."},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+
+            "output": {
+                "Score": [
+                    0.5
+                ],
+                "Label": [
+                    "Positive"
+                ],
+                "Positive words": [
+                    "love"
+                ],
+                "Negative Words": [
+                    ""
+                ]
+            }
+    }
+
+
+def test_get_text_sentiment_null():
+    response = client.post(
+        "/sentiment_analysis",
+        headers={"content-type": "application/json"},
+        json={"sentence": None},
+    )
+
+    assert response.status_code == 422
